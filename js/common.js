b105ea8d2d6d62e5df56d8cd6cc08145e9ff957c
getuidev <103842932+getuidev@users.noreply.github.com>
2022-04-17 10:40:36 +0530

    all pages

---
+++ js/common.js
@@ -0,0 +1,32 @@
+$(document).ready(function () {
+    $(".search-icon").click(function () {
+        $("#headerSearch").fadeToggle();
+    });
+    $("#toggleNav").click(function () {
+        $("#megaMenu").slideToggle("slow", function () {
+            if ($("#megaMenu").is(":visible")) {
+                $("body").addClass("openMenu");
+            } else {
+                $("body").removeClass("openMenu");
+            }
+        });
+    });
+    $(".listItems li").click(function () {
+        if ($(this).hasClass("active")) {
+            $(".listItems li").removeClass("active")
+        } else {
+            $(".listItems li").removeClass("active");
+            $(this).addClass("active");
+        }
+    });
+    setTimeout(function () {
+        $(window).scroll(function () {
+            $(this).scrollTop() > 100 ? $(".scrolltop").fadeIn() : $(".scrolltop")
+                .fadeOut()
+        }), $(".scrolltop").click(function () {
+            return $("html, body").animate({
+                scrollTop: 0
+            }, 600), !1
+        })
+    }, 500)
+});
